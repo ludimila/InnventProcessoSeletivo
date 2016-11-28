a9c5887aac6770bf9bd7d39f7cb4f37734e96bec
Ludimila <lbelacruz@gmail.com>
2016-11-28 11:39:20 -0200

    Removendo arquivos não usados

--- static/index.php
+++
@@ -1,5 +0,0 @@
-<?php header( 'Location: /index.html' ) ;
-	echo '<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">';
-	echo '<link href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">';
-
-?>
